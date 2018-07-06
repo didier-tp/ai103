ea23385f3f9212da0c7b2714778c5464c46afdcb
didier <didier@ici_ou_la.fr>
2018-07-06 12:11:29 +0200

    nouvelle version

--- p1/src/main/java/fr/afcepf/ai103/web/ServletProduit.java
+++ p1/src/main/java/fr/afcepf/ai103/web/ServletProduit.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import fr.afcepf.ai103.tpJdbc.IProductDao;
+import fr.afcepf.ai103.tpJdbc.ProductDaoJdbc;
 import fr.afcepf.ai103.tpJdbc.Produit;
 
 /**
@@ -33,26 +35,42 @@ public class ServletProduit extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String sPrixMaxi = request.getParameter("prixMaxi");
-		double prixMaxi = (sPrixMaxi!=null && !sPrixMaxi.isEmpty())?Double.parseDouble(sPrixMaxi):0.0;
-		
-		
-		//response.setContentType("text/html");
-		response.setContentType("application/json");
+		double prixMaxi = (sPrixMaxi!=null && !sPrixMaxi.isEmpty())?Double.parseDouble(sPrixMaxi):99999999.0;
 		
 		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
 		
+		IProductDao productDao = new ProductDaoJdbc();
+		List<Produit>  listeProduits = productDao.rechercherProduits();
+		
+		List<Produit> listeProdPasCher = new ArrayList<Produit>();
+		for(Produit prod  : listeProduits)
+			if(prod.getPrix() <=  prixMaxi )
+				listeProdPasCher.add(prod);
+		
+		out.println("<html><body>");
+		out.println("<table border='1'>");
+		out.println("<tr><th>numero</th><th>label</th><th>prix</th></tr>");
+		for(Produit p : listeProdPasCher) {
+			out.println("<tr><td>"+p.getNumero()+"</td><td>"+p.getLabel()
+			           +"</td><td>"+p.getPrix()+"</td></tr>");
+		}
+		out.println("</table>");
+		out.println("</body></html>");
+	
+		
+		
+		/*
+		response.setContentType("application/json");
 		Produit prod1 = new Produit(1L,"prod1",1.0);
 		Produit prod2 = new Produit(2L,"prod2",2.0);
 		List<Produit> listeProd = new ArrayList<Produit>();
 		listeProd.add(prod1);  listeProd.add(prod2);
-		/*
-		out.println("<html><body>");
-		out.println("<p>...</p>");
-		out.println("</body></html>");
-		*/
+		
 		ObjectMapper mapper = new ObjectMapper();
 		String listeProdAsJsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(listeProd);
 		out.println(listeProdAsJsonString);
+		*/
 		
 	}
 
