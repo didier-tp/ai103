a3b208393c4eb8a94154d9931bae17b5918f6b90
didier <didier@ici_ou_la.fr>
2018-07-09 11:43:36 +0200

    nouvelle version

--- p1/src/main/java/fr/afcepf/ai103/web/ServletController.java
+++ p1/src/main/java/fr/afcepf/ai103/web/ServletController.java
@@ -1,10 +1,14 @@
 package fr.afcepf.ai103.web;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.afcepf.ai103.web.model.Client;
 
 /*
  * DANS MVC (ou MVC2) : ce servlet joue le rôle de contrôleur principal 
@@ -28,7 +32,25 @@ public class ServletController extends HttpServlet {
 	}
 	
 	protected void doSaveClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	  //...
+	  Client client = new Client();
+	  String pageSuite="/clientOk.jsp"; //"/clientOk.jsp" ou "/saisirClient.jsp" vers lequel faire redirection
+	  String message=""; //éventuel message d'erreur (ex: age invalide)
+	  client.setNom(request.getParameter("nom"));
+	  client.setPrenom(request.getParameter("prenom"));
+	  client.setEmail(request.getParameter("email"));
+	  String sAge = request.getParameter("age");
+	  try {
+		  client.setAge(Integer.parseInt(sAge));
+	  } catch(NumberFormatException ex) {
+		  message=sAge + " = age invalide";
+		  pageSuite = "/saisirClient.jsp";
+	  }
+	  request.setAttribute("client", client); //on stocke dans request ou session des choses
+	  request.setAttribute("message", message); //qui seront accessibles depuis les vues (pages JSP)
+	  RequestDispatcher rd ; 
+	  rd = this.getServletContext().getRequestDispatcher(pageSuite);
+	  rd.forward(request, response); //redirection interne vers page JSP , 
+	  //c'est la page JSP qui continue à générer la page HTML de réponse
 	}
 
 	
