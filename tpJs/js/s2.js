b2ad25b6a8f0f094b7162cf79527e3921f79e85f
didier <didier@ici_ou_la.fr>
2018-07-27 11:37:59 +0200

    nouvelle version

--- tpJs/js/s2.js
+++ tpJs/js/s2.js
@@ -4,7 +4,8 @@ function calculerAddition(){
 		  console.log("a="+a);
 		  var b =  document.getElementById("b").value;
 		  console.log("b="+b);
-		  var res = a+b;
+		  var res = Number(a)+Number(b); // le + déclenche une concaténation entre 2 string
+		                 //ou bien une addition entre 2 choses numériques
 		  
 		  document.getElementById("spanRes").innerHTML = res; 
 		}
