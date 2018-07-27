08f2d61f539eb771d09b1efc82c3998bc9c06b08
didier <didier@ici_ou_la.fr>
2018-07-27 12:16:16 +0200

    nouvelle version

--- tpJs/js/s2.js
+++ tpJs/js/s2.js
@@ -1,11 +1,28 @@
-function calculerAddition(){
+function calculerOperation(pOperation){
 		  
-		  var a =  document.getElementById("a").value;
+		  var a =  document.querySelector("#a").value; //document.getElementById("a").value;
 		  console.log("a="+a);
-		  var b =  document.getElementById("b").value;
+		  var b =   document.querySelector("#b").value;  //document.getElementById("b").value;
 		  console.log("b="+b);
-		  var res = Number(a)+Number(b); // le + déclenche une concaténation entre 2 string
-		                 //ou bien une addition entre 2 choses numériques
 		  
-		  document.getElementById("spanRes").innerHTML = res; 
+		  var res = 0;
+		  if(pOperation == '+'){
+		      res = Number(a)+Number(b); // le + déclenche une concaténation entre 2 string
+		                 //ou bien une addition entre 2 choses numériques
+		    }else if(pOperation == '*'){
+			  res=a*b;
+		   }
+		  
+		  //document.getElementById("spanRes").innerHTML = res;
+          document.querySelector("#spanRes").innerHTML = res;
+		  
+		  document.querySelector(".enEvidence").style.backgroundColor = "yellow";	
+          /* 
+          var tabInputs = document.querySelectorAll("input");
+		  for( index in tabInputs){
+			  if(tabInputs[index] != null)
+			     tabInputs[index].style.backgroundColor = "green";	
+		  }
+          */
+          		  
 		}
