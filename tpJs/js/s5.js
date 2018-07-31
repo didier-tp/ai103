ceb16ffa0cae2a72f6b5900faf2a8c9b4e749a09
didier <didier@ici_ou_la.fr>
2018-07-31 16:01:20 +0200

    nouvelle version

--- tpJs/js/s5.js
+++ tpJs/js/s5.js
@@ -20,13 +20,24 @@ function calculerEtAfficher(){
     var	l=600;
 	var oh = h/2 
 	var ol=l/2;
+	var ech=100;
 	
 	ctx.moveTo(0,oh); ctx.lineTo(l,oh); //axe horizontal
 	ctx.moveTo(ol,0); ctx.lineTo(ol,h); //axe vertical
-	
-	for(x=-10;x<=10;x=x+1){
+	var x1=null;
+	var y1=null;
+		 
+	for(x=-10;x<=10;x=x+0.1){
 		 y = eval(fx);
-		 console.log("x="+x +" ,y=" + y);
+		 
+		 var x2=x * 100 + oh;
+		 var y2= -y * 100 + ol;
+		 console.log("x="+x +" ,y=" + y + ",x1="+x1 +" ,y1=" + y1 + ",x2="+x2 +" ,y2=" + y2);
+		 if(x1!=null){
+			ctx.moveTo(x1,y1);	
+            ctx.lineTo(x2,y2);
+		 }	
+         x1=x2; y1=y2; //for next loop	iteration	 
 	 }
 		
      ctx.closePath();
