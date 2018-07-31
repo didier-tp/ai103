b59fa7422909d8450ee121c8bbab031d1b8ab174
didier <didier@ici_ou_la.fr>
2018-07-31 15:29:26 +0200

    nouvelle version

--- tpJs/js/s5.js
+++ tpJs/js/s5.js
@@ -10,9 +10,25 @@ function calculerEtAfficher(){
 		  
      var fx =  document.querySelector("#fx").value;
 	 var x = 0;
-	 for(x=-10;x<=10;x=x+1){
+	 
+	 
+	 var canvasElement = document.getElementById("idCanvas");
+	 var ctx = canvasElement.getContext("2d");
+		ctx.beginPath();
+		
+	var h=400;
+    var	l=600;
+	var oh = h/2 
+	var ol=l/2;
+	
+	ctx.moveTo(0,oh); ctx.lineTo(l,oh); //axe horizontal
+	ctx.moveTo(ol,0); ctx.lineTo(ol,h); //axe vertical
+	
+	for(x=-10;x<=10;x=x+1){
 		 y = eval(fx);
 		 console.log("x="+x +" ,y=" + y);
 	 }
-		 	  
+		
+     ctx.closePath();
+	 ctx.stroke(); 	  
 }
